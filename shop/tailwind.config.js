2dd25617630d884d0301ea34da42ff3adbb8a3a8
Heiðar Freyr <freyr1980@gmail.com>
2022-03-15 16:43:15 +0000

    Update tailwind.config.js

--- shop/tailwind.config.js
+++ shop/tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js",
     "./src/**/*.{js,jsx,ts,tsx}",
     ":/public/index.html"
   
